ea525db4ed83b7d10a33d10d46c87d64912227b5
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2025-09-11 19:28:13 -0300

    chore: update site domain in astro config from acjr.dev to acjr.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 export default defineConfig({
-    site: 'https://acjr.dev', // Replace with your actual domain
+    site: 'https://acjr.com', // Replace with your actual domain
     adapter: node({
         mode: 'standalone',
         experimentalStaticHeaders: true,
